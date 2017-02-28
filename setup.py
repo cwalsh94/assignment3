ed22ce85e3db461009cea80cc30cb017eecac9a2
Claire Walsh <Claire@dhcp-892bf92e.ucd.ie>
2017-02-28 15:19:00 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(name="solve_led",
+      version="0.1",
+      description="Basic system information for COMP30670",
+      url="",
+    
+      author="Claire Walsh",
+      author_email="claire.walsh2@ucdconnect.ie",
+      licence="GPL3",
+      packages=['Claire'],
+      entry_points={
+          'console_scripts':['solve_led=Claire.main:main']
+          }
+      )
